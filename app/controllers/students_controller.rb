788b0d565b2697b53e126019af34dea36979ab03
Chance Smith <chancesmith97@hotmail.com>
2020-07-01 20:33:29 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -14,7 +14,10 @@ class StudentsController < ApplicationController
 
   def create
     @student = Student.new
-    @student.
+    @student.first_name = params[:first_name]
+    @student.last_name = params[:last_name]
+    @student.save 
+    redirect_to student_path(@student)
   end
 
 end
